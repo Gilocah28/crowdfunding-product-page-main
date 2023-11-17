4e6c4631e256ee0cdb52153027e087a41e3155a3
Abstract Programmer <AbstractProggrammer@example.com>
2023-11-17 16:02:07 +0800

    done style about

--- script/app.js
+++ script/app.js
@@ -2,15 +2,27 @@ const navMobile = document.querySelector('.mobile-link')
 
 const navOpenMenu = document.getElementById('open-menu')
 const navCloseMenu = document.getElementById('close-menu')
+const blur1 = document.querySelector('.blur1')
 
 navOpenMenu.addEventListener('click',()=>{
     navOpenMenu.classList.toggle('active-hide')
     navCloseMenu.classList.toggle('active-show')
     navMobile.classList.toggle('active')
+    blur1.classList.toggle('blur')
 })
 
 navCloseMenu.addEventListener('click',()=>{
     navOpenMenu.classList.toggle('active-hide')
     navCloseMenu.classList.toggle('active-show')
     navMobile.classList.toggle('active')
+    blur1.classList.toggle('blur')
+})
+
+const aboutMobilebtn = document.querySelector('.about')
+
+aboutMobilebtn.addEventListener('click',()=>{
+    navOpenMenu.classList.toggle('active-hide')
+    navCloseMenu.classList.toggle('active-show')
+    navMobile.classList.toggle('active')
+    blur1.classList.remove('blur')
 })
